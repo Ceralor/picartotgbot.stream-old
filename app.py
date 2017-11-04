3dc1a954ea36127f72dd3952f2713cc1e0ab50ed
Ceralor <ceralor@gmail.com>
2017-11-04 02:12:36 -0500

    starting template, welcome

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import json
 app = Flask(__name__)
 
 with open("config.json") as fh:
-	app.config = json.load(fh)
+    app.config = json.load(fh)
 
 bot = Bot(app)
 
@@ -14,30 +14,32 @@ mysql = MySQL()
 mysql.init_app(app)
 
 def get_mysql_cursor():
-	conn = mysql.connect()
-	cursor = conn.cursor()
-	return cursor
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    return cursor
 
 @app.route("/")
 def hello():
-	return render_template("index.html")
+    return render_template("index.html")
 
 @bot.command("start")
 def start_auth(message):
-	cur = get_mysql_cursor()
-	
-	return "Hello"
+    cur = get_mysql_cursor()
+    user_id = message["from"]["id"]
+    cur.callproc('check_tg_user',user_id)
+    
+    return "Hello"
 
 @bot.command("who_online")
 def get_who_online(message):
-	return "No one. Ever."
+    return "No one. Ever."
 
 @bot.command("interact")
 def bot_interact(message):
-	# Get the rest of the message string
-	params_text = bot.find_params(message["text"])
-	if params_text == "":
-		msg = "Hey... You didn't say anything!"
-	else:
-		msg = "Hey, you also said " + params_text
-	return msg
+    # Get the rest of the message string
+    params_text = bot.find_params(message["text"])
+    if params_text == "":
+        msg = "Hey... You didn't say anything!"
+    else:
+        msg = "Hey, you also said " + params_text
+    return msg
